73ac360f2218306667b2faa95f7c91283dfba754
aresmiiro <100922846+aresmiiro@users.noreply.github.com>
2023-01-23 17:23:02 +0100

    Adding search delete and check

--- todo-project/src/app/appUI.js
+++ todo-project/src/app/appUI.js
@@ -1,47 +1,41 @@
 import React from 'react';
 import { CreateTodoButton } from '../createTodoButtons/CreateTodoButton';
+import { TodoContext } from '../todoContext';
 import { TodoCounter } from '../todoCounter/TodoCounter';
 import { TodoItem } from '../todoItem/TodoItem';
 import { TodoList } from '../todoList/TodoList';
 import { TodoSearch } from '../todoSearch/TodoSearch';
 
-
-function AppUI({
-      totalTodos,
-      completedTodos,
-      searchValue,
-      setSearchValue,
-      searchedTodos,
-      completeTodo,
-      deleteTodo,
-}) {
-    return (
-        <React.Fragment>
-      <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-      />
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete = {() => completeTodo(todo.text)}
-            onDelete = {() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
+function AppUI() {
+  const {
+          error,
+          loading,
+          searchedTodos,
+          completeTodo,
+          deleteTodo,
+        } = React.useContext(TodoContext)
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+          <TodoList>
+            {error && <p>Desespérate, hubo un error...</p>}
+            {loading && <p>Estamos cargando, no desesperes...</p>}
+            {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+            
+            {searchedTodos.map(todo => (
+              <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </TodoList>
       <CreateTodoButton />
     </React.Fragment>
-    )
+  );
 }
 
 export { AppUI };
-
--- todo-project/src/todoSearch/TodoSearch.js
+++ todo-project/src/todoSearch/TodoSearch.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { TodoContext } from '../todoContext';
 import './TodoSearch.css';
 
 
 
-function TodoSearch({searchValue, setSearchValue}) {  
+function TodoSearch() {
+    const {searchValue, setSearchValue} = React.useContext(TodoContext);
   const onSearchValueChange = (event) => {
     setSearchValue(event.target.value);
   } ;
